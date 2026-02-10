e4a1c2ab4772b94bcbbef83a300d8160b4e012ca
Chiranjib Mohapatra <chiranjibmohapatra@invesmategroup.com>
2026-02-10 15:08:46 +0530

    valid anagram

--- Strings/valid_anagram.js
+++ Strings/valid_anagram.js
@@ -1,3 +1,7 @@
+// Question Write a function that takes in two strings and returns true if they are anagrams of each other, and false otherwise. An anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+
+// Method -1 
 function validateAnagram(s, t) {
     if (s.length !== t.length) return false;
     const obj = {};
@@ -16,4 +20,17 @@ function validateAnagram(s, t) {
 
 const a = validateAnagram("silent", "listen");
 console.log(a, '0')
+
+
+// Method -2 
+
+function isAnagram(str1, str2){
+    if(str1.length !== str2.length) return false;
+    if(str1.split('').sort().join('') === str2.split('').sort().join('')) return true;
+    return false;
+
+}
+
+let a2 = isAnagram("silent", "listen");
+console.log(a2 , 'Test')
 
