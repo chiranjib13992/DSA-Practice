c7831d299cb5732f2b1492b4360290dbe99fca02
Chiranjib Mohapatra <chiranjibmohapatra@invesmategroup.com>
2026-02-10 15:04:50 +0530

    Reverse String added

---
+++ Strings/reverse_string.js
@@ -0,0 +1,13 @@
+function isReverse(str1, str2) {
+    if (str1.length !== str2.length) return false;
+
+    let i = str1.length - 1;
+    for (let char of str1) {
+        if (str2.at(i).toUpperCase() !== char.toUpperCase()) return false;
+        i--;
+    }
+    return true
+}
+
+let a2 = isReverse("kattak", "kattaK");
+console.log(a2, 'ooo')
