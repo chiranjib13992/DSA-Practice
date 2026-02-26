53a61e94ea33b679b7d85796c523a984fec8bb61
Chiranjib Mohapatra <chiranjib1399@gmail.com>
2026-02-26 19:47:10 +0530

    sorting added

---
+++ Array/sorting.js
@@ -0,0 +1,17 @@
+function isSorted(arr) {
+    if (arr.length <= 1) {
+        return true;
+    }
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < arr[i - 1]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+// Examples
+console.log(isSorted([1, 2, 3, 4, 5])); // true
+console.log(isSorted([1, 3, 2, 4, 5])); // false
